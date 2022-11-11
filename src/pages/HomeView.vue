27ffbcaedce3bda7f22b68d5a6cdab6cfe41cada
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-11 12:52:43 +0700

    implement API get list areas

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <list-areas></list-areas>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ListAreas from "@/components/ListAreas.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    ListAreas,
   },
 };
 </script>
