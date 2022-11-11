06415abd86ecb66dae05b7d27fc3f4e2239e69cd
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-11 09:37:39 +0700

    delete unused components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "../pages/HomeView.vue";
 
 Vue.use(VueRouter);
 
@@ -17,7 +17,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "../pages/AboutView.vue"),
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "not-found",
+    component: () => import("../pages/NotFound.vue"),
   },
 ];
 
