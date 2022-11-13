4b3ae81a20aafa33b89c6daa23fd8494dda4be48
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-13 10:57:04 +0700

    fixing mixing blocked content endpoint

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_KEY = "647bfda4d3fc48a3b3da19d135c57640";
 
 export const HTTP = axios.create({
-  baseURL: `http://api.football-data.org/v2/`,
+  baseURL: `https://api.football-data.org/v2/`,
   headers: {
     "X-Auth-Token": `${API_KEY}`,
   },
