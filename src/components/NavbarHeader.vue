dc5f3268d311d75f5b9dba5fd2b37fb0d5022185
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-13 10:42:05 +0700

    fixing navbar header

--- src/components/NavbarHeader.vue
+++ src/components/NavbarHeader.vue
@@ -2,14 +2,11 @@
   <div>
     <nav>
       <div class="navbar-logo">
-        <h1>Frontend Test</h1>
+        <h1>Footballs Data</h1>
       </div>
       <div class="navbar-link">
         <a href="https://github.com/fjrpambudhi74">
-          <button class="button button-line">
-            <font-awesome-icon :icon="['fab', 'github']" class="myIcon" />&nbsp;
-            GitHub
-          </button>
+          <button class="button button-line">GitHub</button>
         </a>
       </div>
     </nav>
