a2df88bc4d71d738df735c78bf7bc76d76d97914
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-13 10:37:44 +0700

    implement search filter country

--- src/pages/AreasView.vue
+++ src/pages/AreasView.vue
@@ -2,10 +2,18 @@
   <div class="card">
     <div class="card-container">
       <h2 class="card-title">Select By Country</h2>
+      <div class="search-box">
+        <input
+          class="search-input"
+          type="text"
+          v-model="search"
+          placeholder="Search Country"
+        />
+      </div>
       <ul class="card-wrapper">
         <li
           class="card-list"
-          v-for="area in listAreas"
+          v-for="area in filteredData"
           :key="area.id"
           @click="goToPage(area.id)"
         >
@@ -25,10 +33,18 @@ export default {
   data() {
     return {
       listAreas: [],
+      search: "",
     };
   },
   mounted() {
     this.getListAreas();
+  },
+  computed: {
+    filteredData() {
+      return this.listAreas.filter((country) => {
+        return country.name.toLowerCase().includes(this.search.toLowerCase());
+      });
+    },
   },
   methods: {
     async getListAreas() {
@@ -45,3 +61,35 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.search-box {
+  max-width: 500px;
+  position: relative;
+  display: flex;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  margin-top: -20px;
+  margin-bottom: 30px;
+}
+
+.search-input {
+  width: 100%;
+  font-size: 16px;
+  padding: 15px 45px 15px 15px;
+  background-color: #fff;
+  color: #333;
+  border-radius: 6px;
+  border: none;
+  transition: all 0.4s;
+  border: 1px solid #333;
+}
+
+.search-input:focus {
+  border: none;
+  outline: none;
+  box-shadow: 0 1px 12px #dbdbdb;
+}
+</style>
